4652da44f9dc1f22ba4947d363168e23e619c02f
Ryan Fauver <ryan.fauver@gmail.com>
2021-04-26 21:52:53 -0700

    add prettier

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,24 @@
 import { useState } from 'react';
-import Head from 'next/head'
-import styles from '../styles/Home.module.scss'
-import classnames from 'classnames'
+import Head from 'next/head';
+import styles from '../styles/Home.module.scss';
+import classnames from 'classnames';
 
 export default function Home() {
-  const [nodes, setNodes] = useState(["M0,0", "L0,50", "L50,50", "Z"])
+  const [nodes, setNodes] = useState(['M0,0', 'L0,50', 'L50,50', 'Z']);
   const updateNodes = (i, command) => {
-    const newNodes = [...nodes]
-    newNodes[i] = command
-    setNodes(newNodes)
-  }
+    const newNodes = [...nodes];
+    newNodes[i] = command;
+    setNodes(newNodes);
+  };
   const addNode = () => {
-    setNodes([...nodes, ''])
-  }
+    setNodes([...nodes, '']);
+  };
 
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main className={styles.main}>
@@ -28,18 +28,29 @@ export default function Home() {
         </div>
 
         <div className={classnames(styles.section, styles.viewer)}>
-          <svg className={styles.view} viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
-            <path fill="#0070f3" d={nodes.join(' ')} />
+          <svg
+            className={styles.view}
+            viewBox='0 0 100 100'
+            xmlns='http://www.w3.org/2000/svg'
+            preserveAspectRatio='none'
+          >
+            <path fill='#0070f3' d={nodes.join(' ')} />
           </svg>
         </div>
       </main>
     </div>
-  )
+  );
 }
 
 function Node(props) {
   const onInputChange = e => {
-    props.updateNodes(props.index, e.target.value)
-  }
-  return (<input className={styles.node} value={props.command} onChange={onInputChange}/>)
+    props.updateNodes(props.index, e.target.value.trim());
+  };
+  return (
+    <input
+      className={styles.node}
+      value={props.command}
+      onChange={onInputChange}
+    />
+  );
 }
