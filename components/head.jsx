0d22799ea255d7d800379f209a1b0a5fbf8e6f76
Ryan Fauver <ryan.fauver@gmail.com>
2024-01-17 21:48:49 -0700

    remove hardcoded values

--- components/head.jsx
+++ components/head.jsx
@@ -36,21 +36,23 @@ export default function Head({ instructions, svgText }) {
         </style>
       </NextHead>
       {/* Fathom - simple website analytics */}
-      {process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' && (
-        <NextScript>
-          {`(function(f, a, t, h, o, m){
-            a[h]=a[h]||function(){
-              (a[h].q=a[h].q||[]).push(arguments)
-            };
-            o=f.createElement('script'),
-            m=f.getElementsByTagName('script')[0];
-            o.async=1; o.src=t; o.id='fathom-script';
-            m.parentNode.insertBefore(o,m)
-          })(document, window, '//fathom-rfauver.fly.dev/tracker.js', 'fathom');
-          fathom('set', 'siteId', 'RBTTM');
-          fathom('trackPageview');`}
-        </NextScript>
-      )}
+      {process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' &&
+        process.env.FATHOM_DOMAIN &&
+        process.env.FATHOM_ID && (
+          <NextScript>
+            {`(function(f, a, t, h, o, m){
+              a[h]=a[h]||function(){
+                (a[h].q=a[h].q||[]).push(arguments)
+              };
+              o=f.createElement('script'),
+              m=f.getElementsByTagName('script')[0];
+              o.async=1; o.src=t; o.id='fathom-script';
+              m.parentNode.insertBefore(o,m)
+            })(document, window, '${process.env.FATHOM_DOMAIN}', 'fathom');
+            fathom('set', 'siteId', '${process.env.FATHOM_ID}');
+            fathom('trackPageview');`}
+          </NextScript>
+        )}
       {/* / Fathom */}
     </>
   );
