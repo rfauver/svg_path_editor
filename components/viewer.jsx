c43df08814969ba8051f91b934364db0f5caefde
Ryan Fauver <ryan.fauver@gmail.com>
2024-02-04 12:01:12 -0700

    expand svg view box to max coordinate

--- components/viewer.jsx
+++ components/viewer.jsx
@@ -3,20 +3,33 @@ import classnames from 'classnames';
 import styles from '../styles/viewer.module.scss';
 
 export default function Viewer({ commands, fillColor }) {
+  const endPoints = commands.map(command => command.endPoint()).filter(Boolean);
+  const maxCoord = Math.max(...endPoints.flat(), 100);
+
   return (
     <div className={styles.component}>
-      <div className={classnames(styles.coord, styles.coordXMin)}>0</div>
-      <div className={classnames(styles.coord, styles.coordYMin)}>0</div>
-      <div className={classnames(styles.coord, styles.coordXMax)}>100</div>
-      <div className={classnames(styles.coord, styles.coordYMax)}>100</div>
-      <svg
-        className={styles.view}
-        viewBox='0 0 100 100'
-        xmlns='http://www.w3.org/2000/svg'
-      >
-        <path fill={fillColor} d={commands.map(c => c.instruction).join(' ')} />
-        {commands.map(getPathHighlight)}
-      </svg>
+      <div className={styles.yCoords}>
+        <div className={styles.coord}>0</div>
+        <div className={styles.coord}>{maxCoord}</div>
+      </div>
+      <div className={styles.rightWrapper}>
+        <div className={styles.xCoords}>
+          <div className={styles.coord}>0</div>
+          <div className={styles.coord}>{maxCoord}</div>
+        </div>
+
+        <svg
+          className={styles.view}
+          viewBox={`0 0 ${maxCoord} ${maxCoord}`}
+          xmlns='http://www.w3.org/2000/svg'
+        >
+          <path
+            fill={fillColor}
+            d={commands.map(c => c.instruction).join(' ')}
+          />
+          {commands.map(getPathHighlight)}
+        </svg>
+      </div>
     </div>
   );
 }
